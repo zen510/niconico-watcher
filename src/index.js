a6cb47c1e259eefe9e5e5ab752c87549e3b6f538
zen510 <44563532+zen510@users.noreply.github.com>
2019-11-10 23:01:12 +0900

    ローカルにあったものを移動

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const discord = require("discord.js")
+const watchNiconico = require("./niconico")
+const discordcfg = require("../config/cfg")
+const unchi = new watchNiconico()
+
+const main = () => {
+
+    const client = new discord.Client()
+    
+    client.on("ready", () => {
+        console.log("ready")
+       // client.channels.get(CHANNEL_ID).send("Bot Ready.")
+        unchi.start()
+    })
+
+    client.login(discordcfg.TOKEN)
+    
+    let count = 0
+    unchi.on("post",unko => {
+    if (unko.meta.status === 200) {
+        console.log("Successfully acquired data.") 
+        let videotitle = unko.data[0].title
+        let videoURL = `https://www.nicovideo.jp/watch/${unko.data[0].contentId}`
+        oldcount = count
+        count = unko.meta.totalCount
+        if (count > oldcount) client.channels.get(discordcfg.CH_ID).send(`**【動画投稿】**${videotitle}\n${videoURL}`)
+    }
+    else {console.log(`HTTP Status Code:${unko.meta.status}`)}
+    })
+
+}
+main()
