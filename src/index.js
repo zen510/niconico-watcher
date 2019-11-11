f4b9db575f090e0ef1ebb6da5b6bb549589ccf8e
zen510 <44563532+zen510@users.noreply.github.com>
2019-11-12 01:12:18 +0900

    起動時にメッセージを投稿しないように修正 コンソールに現在時刻を表示

--- src/index.js
+++ src/index.js
@@ -13,19 +13,34 @@ const main = () => {
     })
 
     client.login(discordcfg.TOKEN)
-    
+
     let count = 0
+    let isFirst = true
+    let time = new Date()
+    let hours = padding(time.getHours(),2)
+    let minutes = padding(time.getMinutes(),2)
+    let nt = `${hours}:${minutes}`
+    
     unchi.on("post",unko => {
     if (unko.meta.status === 200) {
-        console.log("Successfully acquired data.") 
+        console.log(`${nt} Successfully acquired data.`) 
         let videotitle = unko.data[0].title
         let videoURL = `https://www.nicovideo.jp/watch/${unko.data[0].contentId}`
         oldcount = count
         count = unko.meta.totalCount
-        if (count > oldcount) client.channels.get(discordcfg.CH_ID).send(`**【動画投稿】**${videotitle}\n${videoURL}`)
+        if (count > oldcount) {
+            console.log(`${nt} new video arrived:${videotitle}`)
+            isFirst = false
+            if (isFirst = false) client.channels.get(discordcfg.CH_ID).send(`**【動画投稿】**${videotitle}\n${videoURL}`)
+        }
     }
     else {console.log(`HTTP Status Code:${unko.meta.status}`)}
     })
 
 }
+
+function padding(num,len){
+    return("0000000000" + num).slice(-len)
+}
+
 main()
